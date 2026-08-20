8939800fa487ccadd39b63b695facc31ed99744e
GuReum <ashguuureum@gmail.com>
2026-08-20 17:29:51 +0900

    UnlockedDoor.cs TeleportTrigger.cs로 이름 변경

--- Assets/Scripts/Starlight/Door/TeleportTrigger.cs
+++ Assets/Scripts/Starlight/Door/TeleportTrigger.cs
@@ -1,11 +1,10 @@
 using Unity.Cinemachine;
 using UnityEngine;
 
-public class UnlockedDoor : MonoBehaviour
+public class TeleportTrigger : MonoBehaviour
 {
     [SerializeField] private Transform destinationPoint;
-    [SerializeField] private CinemachineCamera targetCam; // 씬 전환 시 카메라 전환을 위해 추가
-
+    [SerializeField] private CinemachineCamera targetCam;
 
     void OnTriggerEnter2D(Collider2D other)
     {
